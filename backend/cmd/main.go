34582494cf21cfc18746241e74f8bbf09d3cce62
Nenad <nbursa@gmail.com>
2025-03-16 23:59:19 +0100

    Initial commit: Setup monorepo with Next.js frontend and Go backend

---
+++ backend/cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"decentralized-plenum/routes"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	app := fiber.New()
+
+	routes.SetupRoutes(app)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatal(app.Listen(":" + port))
+}
