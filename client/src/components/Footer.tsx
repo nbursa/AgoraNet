1fa70878240742056ccb16011fd10f02c6171c83
Nenad <nbursa@gmail.com>
2025-03-27 12:48:53 +0100

    chore: update footer copy

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import { useTranslations } from "next-intl";
+
 export default function Footer() {
+  const t = useTranslations("common");
+
   return (
     <footer className="bg-gray-900 text-white text-center px-4 py-1 text-xs">
-      &copy; {new Date().getFullYear()} Nenad Bursać. All rights reserved.
+      &copy; {new Date().getFullYear()} {t("copyright")}
     </footer>
   );
 }
